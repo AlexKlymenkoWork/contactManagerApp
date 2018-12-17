dbcea480edad85019bd909a508ea4a81cde3ff9b
Alex Klm <alexcortez333@gmail.com>
2018-12-17 19:37:16 +0200

    Updates

---
+++ precache-manifest.61afc8ad016aba4e051a0dd899ad579d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49fa871e5db4b580492d",
+    "url": "/contactManagerApp/static/js/runtime~main.49fa871e.js"
+  },
+  {
+    "revision": "8994fda68898345acfdb",
+    "url": "/contactManagerApp/static/js/main.8994fda6.chunk.js"
+  },
+  {
+    "revision": "3f0be54e2d76c9dd1a31",
+    "url": "/contactManagerApp/static/js/1.3f0be54e.chunk.js"
+  },
+  {
+    "revision": "8994fda68898345acfdb",
+    "url": "/contactManagerApp/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "3f0be54e2d76c9dd1a31",
+    "url": "/contactManagerApp/static/css/1.3e57731d.chunk.css"
+  },
+  {
+    "revision": "3a00c93fccb015b1ce7a1283239db695",
+    "url": "/contactManagerApp/index.html"
+  }
+];
